6bb6b0ed90b9d0495ba26b5102e106ad48bff496
unknown <tajdaley@yahoo.comgit config --global user.name TajahDaley>
2022-01-01 14:47:43 -0500

    adding to js

--- static/js/app.js
+++ static/js/app.js
@@ -23,3 +23,27 @@ function buildTable(data) {
     );
   });
 }
+
+function handleClick() {
+    //grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    //check to see if a date was entered and filter the
+    //data using the date
+    if (date) {
+        //apply filter to the table data to only leep the
+        //rows where the datetime value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    //rebuild the table using the filtered data
+    //@note:if tno date was entered, then filteredData will
+    //just be the originl tableData.
+    buildTable(filteredData);
+}
+
+//attach am evemt to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+//build the table when the page loads
+buildTable(tableData);
